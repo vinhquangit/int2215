661eacd90ca0adb795c256d5b1f29221b81ebfa0
Vinh Quang Nguyen <vinhquangit01@gmail.com>
2020-04-02 14:15:21 +0700

    added Hangman

---
+++ Hangman/cmake-build-debug/CMakeFiles/Hangman.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hangman.dir/main.cpp.obj"
+  "Hangman.exe"
+  "Hangman.exe.manifest"
+  "Hangman.pdb"
+  "libHangman.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hangman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
